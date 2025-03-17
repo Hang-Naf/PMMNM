00692a4c84668e903a6ac6f9e33994f6fa3ffbbd
Nguyễn Hằng <nguyenthilehang29112004@gmail.com>
2025-03-17 15:23:45 +0700

    Add files via upload

--- resign.php
+++ resign.php
@@ -10,30 +10,26 @@ if (!isset($_SESSION['system'])) {
 		$_SESSION['system'][$k] = $v;
 	}
 }
-
 ob_end_flush();
-
 ?>
 
 <head>
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-	<title><?php echo $_SESSION['system']['name'] ?></title>
+	<title>Đăng ký - <?php echo $_SESSION['system']['name'] ?></title>
 
 	<?php include('./header.php'); ?>
 	<?php
-	if (isset($_SESSION['login_id'])) {
+	if (isset($_SESSION['login_id']))
 		header("location:index.php?page=home");
-	}
-	?>
 
+	?>
 </head>
 <style>
 	body {
 		width: 100%;
 		height: calc(100%);
-		/*background: #007bff;*/
 	}
 
 	main#main {
@@ -42,7 +38,7 @@ ob_end_flush();
 		background: white;
 	}
 
-	#login-right {
+	#register-right {
 		position: absolute;
 		right: 0;
 		width: 40%;
@@ -52,7 +48,7 @@ ob_end_flush();
 		align-items: center;
 	}
 
-	#login-left {
+	#register-left {
 		position: absolute;
 		left: 0;
 		width: 60%;
@@ -60,12 +56,9 @@ ob_end_flush();
 		background: #59b6ec61;
 		display: flex;
 		align-items: center;
-		/*background: url(assets/uploads/blood-cells.jpg);
-	    background-repeat: no-repeat;
-	    background-size: cover;*/
 	}
 
-	#login-right .card {
+	#register-right .card {
 		margin: auto;
 		z-index: 1
 	}
@@ -79,84 +72,76 @@ ob_end_flush();
 		color: #000000b3;
 		z-index: 10;
 	}
-
-	div#login-right::before {
-		content: "";
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: calc(100%);
-		height: calc(100%);
-		/*background: #000000e0;*/
-	}
 </style>
 
 <body>
 
-
 	<main id="main" class=" bg-light">
-		<div id="login-left" class="bg-info">
+		<div id="register-left" class="bg-info">
 			<img src="house.png" alt="..." width="100%">
 		</div>
 
-		<div id="login-right" class="bg-light">
+		<div id="register-right" class="bg-light">
 			<div class="w-100">
 				<h4 class="text-info text-center"><b><?php echo $_SESSION['system']['name'] ?></b></h4>
-				<br>
-				<br>
+				<br><br>
 				<div class="card col-md-8">
 					<div class="card-body">
-						<form id="login-form">
+						<form id="register-form">
+							<div class="form-group">
+								<label for="name" class="control-label">Name</label>
+								<input type="text" id="name" name="name" class="form-control" required>
+							</div>
 							<div class="form-group">
 								<label for="username" class="control-label">Username</label>
-								<input type="text" id="username" name="username" class="form-control">
+								<input type="text" id="username" name="username" class="form-control" required>
 							</div>
 							<div class="form-group">
 								<label for="password" class="control-label">Password</label>
-								<input type="password" id="password" name="password" class="form-control">
+								<input type="password" id="password" name="password" class="form-control" required>
 							</div>
-							<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+							<div class="form-group">
+								<label for="confirm_password" class="control-label">Confirm Password</label>
+								<input type="password" id="confirm_password" name="confirm_password" class="form-control" required>
+							</div>
+							<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Sign Up</button></center>
 						</form>
-						<p>Bạn đã có tài khoản chưa? Nếu chưa hãy ấn <a href="resign.php">vào đây</a>!</p>
+						<p>Đã có tài khoản? <a href="login.php">Đăng nhập tại đây</a>.</p>
 					</div>
 				</div>
 			</div>
 		</div>
 
-
 	</main>
 
 	<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-
 </body>
 <script>
-	$('#login-form').submit(function(e) {
-		e.preventDefault()
-		$('#login-form button[type="button"]').attr('disabled', true).html('Logging in...');
-		if ($(this).find('.alert-danger').length > 0)
-			$(this).find('.alert-danger').remove();
+	$('#register-form').submit(function(e) {
+		e.preventDefault();
+		if ($('#password').val() !== $('#confirm_password').val()) {
+			alert('Mật khẩu không khớp!');
+			return false;
+		}
 		$.ajax({
-			url: 'ajax.php?action=login',
+			url: 'ajax.php?action=signup',
 			method: 'POST',
 			data: $(this).serialize(),
-			error: err => {
-				console.log(err)
-				$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-
-			},
 			success: function(resp) {
+				console.log(resp); // Ghi log phản hồi từ server
 				if (resp == 1) {
-					location.href = 'index.php?page=home';
+					alert('Đăng ký thành công!');
+					location.href = 'login.php';
 				} else if (resp == 2) {
-					location.href = 'user_home.php'; // Staff vào trang staff
+					alert('Tên đăng nhập đã tồn tại.');
 				} else {
-					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
-					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+					alert('Đăng ký thất bại, vui lòng thử lại.');
 				}
 			}
-		})
-	})
+
+		});
+	});
 </script>
 
 </html>
