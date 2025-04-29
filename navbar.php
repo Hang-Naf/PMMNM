90808b3b0ad8d892ba52d011e3e53eade9bddefd
Nguyễn Hằng <nguyenthilehang29112004@gmail.com>
2025-04-29 12:22:42 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,39 +1,136 @@
-<style>
-	.collapse a {
-		text-indent: 10px;
-	}
+<?php include('db_connect.php');?>
 
-	nav#sidebar {
-		top: 55px;
-		/*background: url(assets/uploads/<?php echo $_SESSION['system']['cover_img'] ?>) !important*/
+<div class="container-fluid">
+	
+	<div class="col-lg-12">
+		<div class="row mb-4 mt-4">
+			<div class="col-md-12">
+				
+			</div>
+		</div>
+		<div class="row">
+			<!-- FORM Panel -->
+
+			<!-- Table Panel -->
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header">
+						<b>List of Tenant</b>
+						<span class="float:right"><a class="btn btn-primary btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_tenant">
+					<i class="fa fa-plus"></i> New Tenant
+				</a></span>
+					</div>
+					<div class="card-body">
+						<table class="table table-condensed table-bordered table-hover">
+							<thead>
+								<tr>
+									<th class="text-center">#</th>
+									<th class="">Name</th>
+									<th class="">House Rented</th>
+									<th class="">Monthly Rate</th>
+									<th class="">Outstanding Balance</th>
+									<th class="">Last Payment</th>
+									<th class="text-center">Action</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+								$i = 1;
+								$tenant = $conn->query("SELECT t.*,concat(t.lastname,', ',t.firstname,' ',t.middlename) as name,h.house_no,h.price FROM tenants t inner join houses h on h.id = t.house_id where t.status = 1 order by h.house_no desc ");
+								while($row=$tenant->fetch_assoc()):
+									$months = abs(strtotime(date('Y-m-d')." 23:59:59") - strtotime($row['date_in']." 23:59:59"));
+									$months = floor(($months) / (30*60*60*24));
+									$payable = $row['price'] * $months;
+									$paid = $conn->query("SELECT SUM(amount) as paid FROM payments where tenant_id =".$row['id']);
+									$last_payment = $conn->query("SELECT * FROM payments where tenant_id =".$row['id']." order by unix_timestamp(date_created) desc limit 1");
+									$paid = $paid->num_rows > 0 ? $paid->fetch_array()['paid'] : 0;
+									$last_payment = $last_payment->num_rows > 0 ? date("M d, Y",strtotime($last_payment->fetch_array()['date_created'])) : 'N/A';
+									$outstanding = $payable - $paid;
+								?>
+								<tr>
+									<td class="text-center"><?php echo $i++ ?></td>
+									<td>
+										<?php echo ucwords($row['name']) ?>
+									</td>
+									<td class="">
+										 <p> <b><?php echo $row['house_no'] ?></b></p>
+									</td>
+									<td class="">
+										 <p> <b><?php echo number_format($row['price'],2) ?></b></p>
+									</td>
+									<td class="text-right">
+										 <p> <b><?php echo number_format($outstanding,2) ?></b></p>
+									</td>
+									<td class="">
+										 <p><b><?php echo  $last_payment ?></b></p>
+									</td>
+									<td class="text-center">
+										<button class="btn btn-sm btn-outline-primary view_payment" type="button" data-id="<?php echo $row['id'] ?>" >View</button>
+										<button class="btn btn-sm btn-outline-primary edit_tenant" type="button" data-id="<?php echo $row['id'] ?>" >Edit</button>
+										<button class="btn btn-sm btn-outline-danger delete_tenant" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+									</td>
+								</tr>
+								<?php endwhile; ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<!-- Table Panel -->
+		</div>
+	</div>	
+
+</div>
+<style>
+	
+	td{
+		vertical-align: middle !important;
+	}
+	td p{
+		margin: unset
+	}
+	img{
+		max-width:100px;
+		max-height: 150px;
 	}
 </style>
-
-<nav id="sidebar" class='mx-lt-5 bg-dark'>
-
-	<div class="sidebar-list">
-		<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-tachometer-alt "></i></span> Trang Chủ</a>
-		<a href="index.php?page=categories" class="nav-item nav-categories"><span class='icon-field'><i class="fa fa-th-list "></i></span> Loại Nhà</a>
-		<a href="index.php?page=houses" class="nav-item nav-houses"><span class='icon-field'><i class="fa fa-home "></i></span> Danh sách nhà</a>
-		<a href="index.php?page=tenants" class="nav-item nav-tenants"><span class='icon-field'><i class="fa fa-user-friends "></i></span> Người thuê nhà</a>
-		<a href="index.php?page=invoices" class="nav-item nav-invoices"><span class='icon-field'><i class="fa fa-file-invoice "></i></span> Thanh toán</a>
-		<a href="index.php?page=reports" class="nav-item nav-reports"><span class='icon-field'><i class="fa fa-list-alt "></i></span> Báo cáo</a>
-		<?php if ($_SESSION['login_type'] == 1): ?>
-			<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users "></i></span> Users</a>
-			<!-- <a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs text-danger"></i></span> System Settings</a> -->
-		<?php endif; ?>
-		<a href="index.php?page=maintenance" class="nav-item nav-maintenance"><span class='icon-field'><i class="fa fa-tools "></i></span> Bảo trì</a>
-		<!-- Quản lý lịch hẹn -->
-		<a href="index.php?page=manage_appointments" class="nav-item nav-manage_appointments">
-			<span class='icon-field'><i class="fa fa-calendar"></i></span> Quản lý lịch hẹn
-		</a>
-	</div>
-
-</nav>
 <script>
-	$('.nav_collapse').click(function() {
-		console.log($(this).attr('href'))
-		$($(this).attr('href')).collapse()
+	$(document).ready(function(){
+		$('table').dataTable()
 	})
-	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+	
+	$('#new_tenant').click(function(){
+		uni_modal("New Tenant","manage_tenant.php","mid-large")
+		
+	})
+
+	$('.view_payment').click(function(){
+		uni_modal("Tenants Payments","view_payment.php?id="+$(this).attr('data-id'),"large")
+		
+	})
+	$('.edit_tenant').click(function(){
+		uni_modal("Manage Tenant Details","manage_tenant.php?id="+$(this).attr('data-id'),"mid-large")
+		
+	})
+	$('.delete_tenant').click(function(){
+		_conf("Are you sure to delete this Tenant?","delete_tenant",[$(this).attr('data-id')])
+	})
+	
+	function delete_tenant($id){
+		start_load()
+		$.ajax({
+			url:'ajax.php?action=delete_tenant',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Data successfully deleted",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
 </script>
